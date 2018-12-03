729ac2293c2f3564ae4dfe1f9f8478464d4ea515
yinyao <yinyaowhy@gmail.com>
2018-12-03 19:42:08 +0800

    today

--- manage.py
+++ manage.py
@@ -1,9 +1,6 @@
-from flask import Flask
-from flask import render_template
 from flask_script import Manager, Server
 
-# from console.console01 import create_apps
-from urls import create_app, test
+from apps import create_app
 
 app = create_app()
 '''
@@ -11,16 +8,7 @@ app = create_app()
 第二步 实例化
 '''
 manager = Manager(app=app)
-manager.add_command('start', Server(port=6000, use_debugger=True))
-
-
-# 控制层
-# 路由系统 配置首页
-@test.route('/login/')
-def login():
-    return render_template('login.html')
-    # return 'hello'
-
+manager.add_command('start', Server(port=6000))
 
 if __name__ == '__main__':
     manager.run()
